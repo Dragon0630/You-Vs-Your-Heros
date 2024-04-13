9dd2f059223b078a4c5817658ef051161cd798a5
Patrick Roberts <Patrick-M-Roberts@protonmail.com>
2024-04-13 08:09:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/You-Vs-Your-Heros/")
 def home():
-    return render_template("index.html")
+    return send_from_directory('html', 'index.html')
 
 @app.route('/You-Vs-Your-Heros/modify', methods = ['POST'])
 def update_text():
