663f8069e9efce403ccfba37ac3ecdb20ff0697f
Bryce Ebsen <158350057+Dragon0630@users.noreply.github.com>
2024-04-12 22:00:50 -0500

    added more stuff for post

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request
+import classes.modify_json as JSON
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("base.html")
 
-@app.route('/modify', methods = ['POST'])
+@app.route('/modify')
 def update_text():
-    data = request.get_json
+    if request.method == 'POST':
+        name = request.form['name']
+        squat = request.form['squat']
+    #         var curl = document.getElementById('curl').value;
+    # var squat = document.getElementById('squat').value;
+    # var bench = document.getElementById('bench').value;
+    # var run = document.getElementById('run').value;
+    # var swim = document.getElementById('swim').value;
+    # var climb = document.getElementById('climb').value;
+    # var sprint = document.getElementById('sprint').value;
+    # var jump = document.getElementById('jump').value;
+    # var reaction = document.getElementById('reaction').value;
+        # Do something with the data (e.g., save it to a JSON file)
+        response_data = {'message': 'Data received successfully', 'name': name}
+        return jsonify(response_data), 200
+    else:
+        return jsonify({'error': 'Method not allowed'}), 405
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ classes/modify_json.py
@@ -0,0 +1,7 @@
+from flask import request
+
+class modify_json:
+    def __init__(data):
+        print(data)
+        print("hello world")
+    
